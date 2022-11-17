a5807800dad0bbcbe8f2221831e963c0256d7ad5
Sasha Munroe <sashalillianmunroe@gmail.com>
2022-11-16 22:01:58 -0500

    Added the HTML

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from "react";
-
+import Weather from "./Weather"
 import './App.css';
 
 export default function App() {
   return (
     <div className="App">
     <div className="container">
-    <h1>Weather App</h1>
+    <Weather />
     <footer>
   This project was created by <a href ="https://www.linkedin.com/in/sasha-munroe-446a14a1/" target="_blank">Sasha Munroe</a> and is {""}
   <a href = "https://github.com/smunroe34/react-assignment"
