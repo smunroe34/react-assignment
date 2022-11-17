ea0c2a18a47ad275e51137b94aac96ae42ddba4b
Sasha Munroe <sashalillianmunroe@gmail.com>
2022-11-16 20:26:05 -0500

    Added footer

--- src/App.js
+++ src/App.js
@@ -6,6 +6,13 @@ export default function App() {
   return (
     <div className="App">
     Weather App
+    <footer>
+  This project was created by Sasha Munroe and is {""}
+  <a href = "https://github.com/smunroe34/react-assignment"
+  target= "_blank">
+    open-sourced on Github
+  </a>
+  </footer>
     </div>
   );
 }
