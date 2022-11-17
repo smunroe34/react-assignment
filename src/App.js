4b009866217d3501bddd5480381017c651ea68dd
Sasha Munroe <sashalillianmunroe@gmail.com>
2022-11-16 20:32:14 -0500

    Added GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -5,9 +5,9 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-    Weather App
+    <h1>Weather App</h1>
     <footer>
-  This project was created by Sasha Munroe and is {""}
+  This project was created by <a href ="https://www.linkedin.com/in/sasha-munroe-446a14a1/" target="_blank">Sasha Munroe</a> and is {""}
   <a href = "https://github.com/smunroe34/react-assignment"
   target= "_blank">
     open-sourced on Github
