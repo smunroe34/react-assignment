25879c3ca8a221951d041b0c54e856fa95131501
Sasha Munroe <sashalillianmunroe@gmail.com>
2022-11-18 21:37:18 -0500

    To prevent security vulnerabilities

--- src/App.js
+++ src/App.js
@@ -6,11 +6,11 @@ export default function App() {
   return (
     <div className="App">
     <div className="container">
-    <Weather />
+    <Weather defaultCity ="Toronto"/>
     <footer>
-  This project was created by <a href ="https://www.linkedin.com/in/sasha-munroe-446a14a1/" target="_blank">Sasha Munroe</a> and is {""}
+  This project was created by <a href ="https://www.linkedin.com/in/sasha-munroe-446a14a1/" target="_blank" rel="noreferrer">Sasha Munroe</a> and is {""}
   <a href = "https://github.com/smunroe34/react-assignment"
-  target= "_blank">
+  target= "_blank" rel="noreferrer">
     open-sourced on Github
   </a>
   </footer>
